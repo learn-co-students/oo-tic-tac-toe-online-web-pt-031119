d9b91a3f14b54da642dfd81ba082d993a72eb55d
github username <github email address>
2019-06-09 14:42:03 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,78 +1,140 @@
-require 'pry'
-
 class TicTacToe
 
   WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5], # Middle row
-  [6,7,8], # Bottom row
-  [0,3,6], # Left column
-  [1,4,7], # Middle column
-  [2,5,8], # Right column
-  [0,4,8], # Left to right down diag.
-  [2,4,6], # Left to right up diag.
-]
-  def initialize#(board = nil)  board ||
-    @board = Array.new(9, " ")
-  end
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+  ]
 
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+def initialize
+  @board = Array.new(9, " ")
+end
 
-  def input_to_index(input)
-    input.to_i - 1
-  end
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
 
-  def move(int, token = "X")
-    @board[int] = token
-  end
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
 
-  def position_taken?(index)
-    @board[index] != " "
-  end
+def move(index, current_player = "X")
+  @board[index] = current_player
+end
 
-  def valid_move?(index)
-    if index == 0..8
-      position_taken?(index) ? false : true
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+end
+
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
+
+def turn_count
+  turn = 0
+  @board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
     end
   end
+  return turn
+end
 
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
+def current_player
+  num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
+
+def turn
+  puts "Please choose a number from 1 to 9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    player_token = current_player
+    move(index, player_token)
+    display_board
+  else
+    turn
+  end
+end
+
+def won?
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
+end
+
+def full?
+  @board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?
+  if !won? && full?
+    return true
+  else
+    return false
+  end
+end
+
+def over?
+  if won? || draw?
+    return true
+  else
+    return false
+  end
+end
+
+def winner
+  index = []
+  index = won?
+  if index == false
+    return nil
+  else
+    if @board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
+def play
+  until over? == true
+    turn
   end
 
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
   end
-
-  def turn(input)
-    input = gets.chomp
-  end
-
 end
 
-
-=begin
-board = Array.new(9, " ") # Creates an array with 9 elements filled with " "
-
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
 end
-
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-=end
